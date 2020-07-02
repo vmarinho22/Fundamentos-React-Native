27edefba74d6cf3bd572d3f212376cb0317b2fbf
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2020-07-02 06:36:23 -0300

    Aprendendo a usar o Console.warn

--- src/components/Primeiro.js
+++ src/components/Primeiro.js
@@ -3,4 +3,10 @@ import { Text } from 'react-native'
 import Estilo from './estilo'
 
 
-export default() => <Text style={Estilo.fontG}>Primeiro!</Text>
+export default() => {
+    console.warn('Opa!')
+    return( 
+        <Text style={Estilo.fontG}>Primeiro!</Text>
+    )
+    
+}
